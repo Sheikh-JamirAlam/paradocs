77e9f1ad15ac840a117c97b13a0397f052426a7d
Sheikh-JamirAlam <alamjamir15@gmail.com>
2025-02-05 01:15:21 +0530

    add web socket

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,12 +1,29 @@
+import { WebSocket, WebSocketServer } from "ws";
 import http from "http";
 
-async function init() {
-  const httpServer = http.createServer();
-  const PORT = process.env.PORT ? process.env.PORT : 8000;
+const server = http.createServer(function (request: any, response: any) {
+  console.log(new Date() + " Received request for " + request.url);
+  response.end("hi there");
+});
 
-  httpServer.listen(PORT, () => {
-    console.log("HTTP Server started listening on port: " + PORT);
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", function connection(ws) {
+  ws.on("error", console.error);
+
+  ws.on("message", function message(data, isBinary) {
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data, { binary: isBinary });
+      }
+    });
   });
-}
 
-init();
+  ws.send("Hello! Message from server!!");
+});
+
+const PORT = process.env.PORT ? process.env.PORT : 8080;
+
+server.listen(PORT, () => {
+  console.log("HTTP Server started listening on port: " + PORT);
+});
