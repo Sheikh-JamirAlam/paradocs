b6c3145a7b9eb2693d04774d9f4b5b12fee69fce
Sheikh-JamirAlam <alamjamir15@gmail.com>
2025-02-11 14:40:00 +0530

    Text style added

--- apps/web/app/components/Document/Toolbar.tsx
+++ apps/web/app/components/Document/Toolbar.tsx
@@ -1,17 +1,118 @@
 "use client";
 
+import { useState } from "react";
 import { Editor } from "@tiptap/react";
-import { Bold, Italic, Underline, List, ListOrdered, CheckSquare, Heading1, Heading2, Heading3, AlignLeft, AlignCenter, AlignRight, AlignJustify } from "lucide-react";
+import { Bold, Italic, Underline, List, ListOrdered, CheckSquare, AlignLeft, AlignCenter, AlignRight, AlignJustify, ChevronDownIcon } from "lucide-react";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 
 interface ToolbarProps {
   editor: Editor | null;
+}
+
+function HeadingSelect({ editor }: ToolbarProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  if (!editor) return null;
+
+  return (
+    <div className="flex gap-1">
+      <DropdownMenu onOpenChange={setIsOpen}>
+        <DropdownMenuTrigger className={`w-[130px] px-2 py-1 flex items-center justify-between font-medium outline-0 ${isOpen ? "bg-gray-300" : "hover:bg-gray-300"} rounded-md cursor-pointer`}>
+          {editor.isActive("heading", { level: 1 }) ? "Heading 1" : editor.isActive("heading", { level: 2 }) ? "Heading 2" : editor.isActive("heading", { level: 3 }) ? "Heading 3" : "Normal Text"}
+          <ChevronDownIcon size={16} />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem onClick={() => editor.chain().focus().setParagraph().run()} className="px-4 py-2 text-base border-b border-gray-200 rounded-b-none cursor-pointer">
+            Normal Text
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()} className="px-4 py-2 text-[32px] border-b border-gray-200 rounded-b-none cursor-pointer">
+            Heading 1
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()} className="px-4 py-2 text-2xl border-b border-gray-200 rounded-b-none cursor-pointer">
+            Heading 2
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()} className="px-4 py-2 text-xl cursor-pointer">
+            Heading 3
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+}
+
+function FontFamilySelect({ editor }: ToolbarProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const fontFamilies = ["Arial", "Times New Roman", "Inter", "'Comic Sans MS', 'Comic Sans'", "cursive", "Courier New", "Verdana"];
+  if (!editor) return null;
+
+  return (
+    <div className="flex gap-1">
+      <DropdownMenu onOpenChange={setIsOpen}>
+        <DropdownMenuTrigger className={`w-[130px] px-2 py-1 flex items-center justify-between font-medium outline-0 rounded-md cursor-pointer ${isOpen ? "bg-gray-300" : "hover:bg-gray-300"}`}>
+          Font Family
+          <ChevronDownIcon size={16} />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          {fontFamilies.map((family) => (
+            <DropdownMenuItem key={family} onClick={() => editor.chain().focus().setFontFamily(family).run()} className="px-4 py-2 cursor-pointer" style={{ fontFamily: family }}>
+              {family}
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+}
+
+function AlignSelect({ editor }: ToolbarProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  if (!editor) return null;
+
+  return (
+    <div className="flex gap-1">
+      <DropdownMenu onOpenChange={setIsOpen}>
+        <DropdownMenuTrigger className={`w-[40px] px-1 py-2 flex items-center justify-between outline-0 ${isOpen ? "bg-gray-300" : "hover:bg-gray-300"} rounded-md cursor-pointer`}>
+          {editor.isActive({ textAlign: "justify" }) ? (
+            <AlignJustify size={16} className="text-black" />
+          ) : editor.isActive({ textAlign: "right" }) ? (
+            <AlignRight size={16} className="text-black" />
+          ) : editor.isActive({ textAlign: "center" }) ? (
+            <AlignCenter size={16} className="text-black" />
+          ) : (
+            <AlignLeft size={16} className="text-black" />
+          )}
+          <ChevronDownIcon size={16} />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem onClick={() => editor.chain().focus().setTextAlign("left").run()} className="px-2 py-2 border-b border-gray-200 rounded-b-none cursor-pointer">
+            <AlignLeft size={16} className="text-black" />
+            Align Left
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => editor.chain().focus().setTextAlign("center").run()} className="px-2 py-2 border-b border-gray-200 rounded-b-none cursor-pointer">
+            <AlignCenter size={16} className="text-black" />
+            Align Center
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => editor.chain().focus().setTextAlign("right").run()} className="px-2 py-2 border-b border-gray-200 rounded-b-none cursor-pointer">
+            <AlignRight size={16} className="text-black" />
+            Align Right
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => editor.chain().focus().setTextAlign("justify").run()} className="px-2 py-2 cursor-pointer">
+            <AlignJustify size={16} className="text-black" />
+            Justify
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
 }
 
 export default function Toolbar({ editor }: ToolbarProps) {
   if (!editor) return null;
 
   return (
-    <div className="mt-4 px-4 py-1 flex gap-1 items-center bg-slate-200 rounded-full">
+    <div className="mt-4 px-4 py-1 flex items-center bg-slate-200 rounded-full">
+      <HeadingSelect editor={editor} />
+      <div className="w-[1px] h-5 mx-2 bg-gray-400" />
+      <FontFamilySelect editor={editor} />
       <div className="w-[1px] h-5 mx-2 bg-gray-400" />
       <div className="flex gap-1">
         <button onClick={() => editor.chain().focus().toggleBold().run()} className={`p-2 rounded-md cursor-pointer ${editor.isActive("bold") ? "bg-gray-300" : "hover:bg-gray-300"}`}>
@@ -26,6 +127,7 @@ export default function Toolbar({ editor }: ToolbarProps) {
       </div>
       <div className="w-[1px] h-5 mx-2 bg-gray-400" />
       <div className="flex gap-1">
+        <AlignSelect editor={editor} />
         <button onClick={() => editor.chain().focus().toggleBulletList().run()} className={`p-2 rounded-md cursor-pointer ${editor.isActive("bulletList") ? "bg-gray-300" : "hover:bg-gray-300"}`}>
           <List size={16} />
         </button>
@@ -34,53 +136,6 @@ export default function Toolbar({ editor }: ToolbarProps) {
         </button>
         <button onClick={() => editor.chain().focus().toggleTaskList().run()} className={`p-2 rounded-md cursor-pointer ${editor.isActive("taskList") ? "bg-gray-300" : "hover:bg-gray-300"}`}>
           <CheckSquare size={16} />
-        </button>
-      </div>
-      <div className="flex gap-1">
-        {/* Headings */}
-        <button
-          onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
-          className={`p-2 rounded-md cursor-pointer ${editor.isActive("heading", { level: 1 }) ? "bg-gray-300" : "hover:bg-gray-300"}`}
-        >
-          <Heading1 size={16} />
-        </button>
-        <button
-          onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
-          className={`p-2 rounded-md cursor-pointer ${editor.isActive("heading", { level: 2 }) ? "bg-gray-300" : "hover:bg-gray-300"}`}
-        >
-          <Heading2 size={16} />
-        </button>
-        <button
-          onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
-          className={`p-2 rounded-md cursor-pointer ${editor.isActive("heading", { level: 3 }) ? "bg-gray-300" : "hover:bg-gray-300"}`}
-        >
-          <Heading3 size={16} />
-        </button>
-
-        {/* Text Alignment */}
-        <button
-          onClick={() => editor.chain().focus().setTextAlign("left").run()}
-          className={`p-2 rounded-md cursor-pointer ${editor.isActive({ textAlign: "left" }) ? "bg-gray-300" : "hover:bg-gray-300"}`}
-        >
-          <AlignLeft size={16} />
-        </button>
-        <button
-          onClick={() => editor.chain().focus().setTextAlign("center").run()}
-          className={`p-2 rounded-md cursor-pointer ${editor.isActive({ textAlign: "center" }) ? "bg-gray-300" : "hover:bg-gray-300"}`}
-        >
-          <AlignCenter size={16} />
-        </button>
-        <button
-          onClick={() => editor.chain().focus().setTextAlign("right").run()}
-          className={`p-2 rounded-md cursor-pointer ${editor.isActive({ textAlign: "right" }) ? "bg-gray-300" : "hover:bg-gray-300"}`}
-        >
-          <AlignRight size={16} />
-        </button>
-        <button
-          onClick={() => editor.chain().focus().setTextAlign("justify").run()}
-          className={`p-2 rounded-md cursor-pointer ${editor.isActive({ textAlign: "justify" }) ? "bg-gray-300" : "hover:bg-gray-300"}`}
-        >
-          <AlignJustify size={16} />
         </button>
       </div>
     </div>
--- apps/web/app/documents/[documentId]/page.tsx
+++ apps/web/app/documents/[documentId]/page.tsx
@@ -8,6 +8,8 @@ import Underline from "@tiptap/extension-underline";
 import TaskList from "@tiptap/extension-task-list";
 import TaskItem from "@tiptap/extension-task-item";
 import TextAlign from "@tiptap/extension-text-align";
+import TextStyle from "@tiptap/extension-text-style";
+import FontFamily from "@tiptap/extension-font-family";
 import useSocket from "../../components/SocketHook";
 import Toolbar from "../../components/Document/Toolbar";
 import { PreserveSpaces } from "../../lib/extensions/tiptap";
@@ -17,7 +19,7 @@ export default function Page() {
   const { content, updateContent } = useSocket();
 
   const editor = useEditor({
-    extensions: [StarterKit, Underline, TaskList, TaskItem.configure({ nested: true }), TextAlign.configure({ types: ["heading", "paragraph"] }), PreserveSpaces],
+    extensions: [StarterKit, Underline, TextStyle, FontFamily, TaskList, TaskItem.configure({ nested: true }), TextAlign.configure({ types: ["heading", "paragraph"] }), PreserveSpaces],
     editorProps: {
       attributes: {
         style: `padding-left: 51px; padding-right: 51px;`,
