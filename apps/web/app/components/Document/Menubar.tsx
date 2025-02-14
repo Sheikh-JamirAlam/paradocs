bbd548a996e20b9d2a8bc2d2fe780c5021826bf9
Sheikh-JamirAlam <alamjamir15@gmail.com>
2025-02-14 23:25:06 +0530

    Update menubar doc name

--- apps/web/app/components/Document/Menubar.tsx
+++ apps/web/app/components/Document/Menubar.tsx
@@ -1,14 +1,29 @@
 "use client";
 
+import { useRef, useEffect, useState } from "react";
 import { CloudDone } from "../Icons";
 
 export default function Menubar() {
+  const [docName, setDocName] = useState("Document Name");
+  const [inputWidth, setInputWidth] = useState(108);
+  const spanRef = useRef<HTMLSpanElement>(null);
+
+  useEffect(() => {
+    if (spanRef.current) {
+      const width = spanRef.current.offsetWidth;
+      setInputWidth(width);
+    }
+  }, [docName]);
+
   return (
-    <section className="w-full px-4 py-2 flex">
+    <section className="w-full px-4 py-2 flex gap-2">
       <div className="text-4xl font-semibold font-serif underline">Paradocs</div>
       <div className="flex flex-col">
-        <div className="font-medium flex gap-2">
-          <input className="w-fit" type="text" value="Document Name" readOnly />
+        <div className="font-medium flex items-center gap-1">
+          <span ref={spanRef} className="px-2 invisible absolute whitespace-pre" aria-hidden="true">
+            {docName}
+          </span>
+          <input className="w-fit px-2" onChange={(e) => setDocName(e.target.value)} type="text" value={docName} style={{ width: `${inputWidth}px` }} />
           <CloudDone />
         </div>
       </div>
